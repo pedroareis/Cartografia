eed9c72b622d8713c29975d813a3c2dfb0d3e17b
pedroareis <pamaralreis@gmail.com>
2020-06-10 18:22:05 -0300

    add mask over continents

--- Scripts/WindStress.py
+++ Scripts/WindStress.py
@@ -6,8 +6,8 @@ from global_land_mask import globe
 import cartopy.feature as cfeature
 from cartopy.util import add_cyclic_point
 
-u10 = Dataset('/Users/Pedro/Documents/DadosNC/u10_8090.nc')
-v10 = Dataset('/Users/Pedro/Documents/DadosNC/v10_8090.nc')
+u10 = Dataset('/Users/Pedro/Documents/DadosNC/u10_1019.nc')
+v10 = Dataset('/Users/Pedro/Documents/DadosNC/v10_1019.nc')
 
 u10w = u10.variables['uwnd'][0]
 v10w = v10.variables['vwnd'][0]
@@ -23,14 +23,15 @@ CD = 0.0013
 t = CD * wind_speed
 
 ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_extent([-75, 20, -75, 10])
+ax.add_feature(cfeature.LAND, zorder=100, edgecolor='k')
+ax.set_extent([-80, 30, -75, 15])
 cyclic_t, cyclic_lon = add_cyclic_point(t, coord=lon)
 ax_cf = plt.contourf(cyclic_lon, lat, cyclic_t, 20, cmap='RdBu_r')
 plt.colorbar(ax_cf, orientation='horizontal', shrink=0.8, pad=.09, label='Pa')
 ax_q = ax.quiver(lon, lat, u10w, v10w)
-ax_T = plt.title('Mean Wind Stress\n1980-90')
+ax_T = plt.title('Mean Wind Stress\n2010-2019')
 ax.gridlines()
 ax.coastlines()
 
-plt.savefig('/Users/Pedro/Documents/DadosNC/teste.png', dpi=300)
+#plt.savefig('/Users/Pedro/Documents/DadosNC/teste.png', dpi=300)
 plt.show()
